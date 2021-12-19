d75bb9c74b09261082ef2a053c9fb4ef2c6f3da1
矢野大樹 <daikitech0123@gmail.com>
2021-12-19 16:47:25 +0900

    add POST mthod

--- http-client/main.go
+++ http-client/main.go
@@ -3,21 +3,37 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
 	//GET
-	res, _ := http.Get("https://example.com")
-	fmt.Println(res.StatusCode)
-	fmt.Println(res.Proto)
-	fmt.Println(res.Header["Date"])
-	fmt.Println(res.Header["Content-Type"])
-	fmt.Println(res.Request.Method)
-	fmt.Println(res.Request.URL)
+	//res, _ := http.Get("https://example.com")
+	//fmt.Println(res.StatusCode)
+	//fmt.Println(res.Proto)
+	//fmt.Println(res.Header["Date"])
+	//fmt.Println(res.Header["Content-Type"])
+	//fmt.Println(res.Request.Method)
+	//fmt.Println(res.Request.URL)
+	//
+	//defer res.Body.Close()
+	//body, _ := ioutil.ReadAll(res.Body)
+	//fmt.Print(string(body))
 
+	// POST
+	vs := url.Values{}
+	vs.Add("id", "1")
+	vs.Add("message", "メッセージ")
+	fmt.Println(vs.Encode())
+	res, err := http.PostForm("https://example.com", vs)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
 	fmt.Print(string(body))
+	fmt.Println("hey")
 
 }
